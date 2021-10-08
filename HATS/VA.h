fdec5f30e4eb2e1fbaf6fc31fba35c54318f752d
jzh <582539856@qq.com>
2021-10-08 11:45:31 +0800

    10.8 lab

--- HATS/VA.h
+++ HATS/VA.h
@@ -15,8 +15,7 @@
 
 using namespace std;
 
-extern mutex active_mutex;
-extern mutex fifo_mutex;
+mutex fifo_mutex;
 
 struct Edge {
     int u;
@@ -27,31 +26,100 @@ struct Edge {
 
 template <typename T>
 class VA {
-private:
-    vector<int> offset;
-    vector<int> neighbor;
-    vector<bool> *active_bits;
-    vector<T> vertex_data;
-    bool isPush;
+    private:
+        vector<int> offset;
+        vector<int> neighbor;
+        vector<bool> *active_bits;
+        vector<T> vertex_data;
+        bool isPush;
 
-    int current_vid;
-    int last_vid;
+        int current_vid;
+        int last_vid;
 
-    queue<Edge> FIFO;
+        queue<Edge> FIFO;
 
-    thread threads[THREAD_NUM];
-private:
-    int scan();
-    void fetch_offsets(int vid, int &start_offset, int &end_offset);
-    vector<Edge> fetch_neighbors(int vid, int start_offset, int end_offset);
-    T prefetch(int vid);
-    void start();
-public:
-    VA();
-    ~VA();
-    void hats_configure(vector<int> _offset, vector<int> _neighbor, vector<bool> *_active, vector<T> _vertex_data, bool _isPush,
-                        int _start_v, int _end_v);
-    Edge hats_fetch_edges();
+        thread threads[THREAD_NUM];
+
+    private:
+        int scan()
+        {
+            for (int i = current_vid; i < last_vid; i++) {
+                if ((*active_bits)[i]) {
+                    (*active_bits)[i] = false;
+                    current_vid++;
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        void fetch_offsets(int vid, int &start_offset, int &end_offset)
+        {
+            start_offset = offset[vid];
+            end_offset = offset[vid + 1];
+        }
+
+        vector<Edge> fetch_neighbors(int vid, int start_offset, int end_offset)
+        {
+            vector <Edge> neighbors;
+            for (int i = start_offset; i < end_offset; ++i) {
+                while (this->FIFO.size() > MAX_DEPTH)
+                    this_thread::yield();               //fifo满时HATS停止
+                lock_guard<mutex> lock(fifo_mutex);
+                FIFO.push(Edge(vid, neighbor[i]));
+                neighbors.emplace_back(vid, i);
+            }
+            return neighbors;
+        }
+
+        T prefetch(int vid)
+        {
+            return vertex_data[vid];
+        }
+
+        void start()
+        {
+            int vid, start_offset, end_offset;
+            vector <Edge> edges;
+            while (current_vid < last_vid) {
+                vid = scan();
+                fetch_offsets(vid, start_offset, end_offset);
+                edges = fetch_neighbors(vid, start_offset, end_offset);
+            }
+            current_vid++;
+        }
+
+    public:
+        VA(){};
+        ~VA()= default;
+
+        void hats_configure(vector<int> _offset, vector<int> _neighbor, vector<bool> *_active, vector<T> _vertex_data, bool _isPush,
+                            int _start_v, int _end_v)
+        {
+            offset = _offset;
+            neighbor = _neighbor;
+            active_bits = _active;
+            vertex_data = _vertex_data;
+            isPush = _isPush;
+            current_vid = _start_v;
+            last_vid = _end_v;
+            start();
+            //thread
+        }
+
+        void hats_fetch_edges(Edge &edge)
+        {
+            while (this->FIFO.empty() && current_vid <= last_vid)
+                this_thread::yield();           //fifo为空时fetch停止
+            lock_guard<mutex> lock(fifo_mutex);
+            if (!FIFO.empty())
+            {
+                edge = FIFO.front();
+                FIFO.pop();
+            }
+            else
+                edge = Edge(-1, -1);
+        }
 };
 
 
--- HATS/test.cpp
+++ HATS/test.cpp
@@ -4,27 +4,56 @@
 
 #include "VA.h"
 #include <cstdlib>
+#include <thread>
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
-void generate(vector<int> &offsets, vector<int> &neighbors, vector<int> &values)
+vector<int> offsets, neighbors, values;
+
+void generate()
 {
-    int vnum = 10, cnt = 0;
+    srand((int) (time(NULL)));
+    int vnum = 20, cnt = 0;
     double r;
     offsets.push_back(0);
     for (int i = 0; i < vnum; ++i) {
         for (int j = 0; j < vnum; ++j) {
             if (i == j)
                 continue;
-            r = rand() / RAND_MAX;
-            if (r < 0.3) {
+            r = (double) rand() / RAND_MAX;
+            if (r < 0.1) {
                 neighbors.push_back(j);
+                values.push_back(i);
                 cnt++;
+//                printf("%d,%d\n", i, j);
             }
         }
         offsets.push_back(cnt);
     }
+}
+
+void fetch(VA<int> &VAs, int start, int end, int &num)
+{
+    Edge edge(1, 1);
+    while (edge.v != -1) {
+        VAs.hats_fetch_edges(edge);
+        printf("(%d,%d):%d, %d\n", start, end, edge.u, edge.v);
+        num++;
+    }
+    num--;
+}
+
+void traverse(vector<bool> *active, vector<int> vertex_data, bool isPush, int start_id, int end_id)
+{
+    VA<int> VAs;
+    int num = 0;
+    thread wthread(&VA<int>::hats_configure, &VAs, offsets, neighbors, active, vertex_data, isPush, start_id, end_id);
+    thread rthread(fetch, ref(VAs), start_id, end_id, ref(num));
+    wthread.join();
+    rthread.join();
+    printf("wt(%d,%d) end, num=%d\n", start_id, end_id, num);
 }
 
 int main()
@@ -32,11 +61,27 @@ int main()
 //    vector<int> offsets = {0, 2, 4, 7, 9};
 //    vector<int> neighbors = {0, 1, 1, 2, 0, 2, 3, 1, 3};
 //    vector<int> values = {1, 7, 2, 8, 5, 3, 9, 6, 4};
-    vector<int> offsets, neighbors, values;
-    generate(offsets, neighbors, values);
+
+    generate();
+
     for (int i:offsets)
         cout << i << " ";
     cout << endl;
     for (int i:neighbors)
         cout << i << " ";
+    printf("\n---------------\n");
+    vector<bool> active(offsets.size() - 1, true);
+    vector<int> vertex_data;
+    for (int i = 0; i < active.size(); ++i)
+        vertex_data.push_back(100 * i);
+    bool isPush = true;
+    thread t[4];
+    for (int i = 0; i < 4; ++i) {
+        t[i] = thread(traverse, &active, vertex_data, isPush, i * 5, (i + 1) * 5);
+    }
+    printf("\nwaiting...\n");
+    for (auto & i : t) {
+        i.join();
+    }
+    return 0;
 }
